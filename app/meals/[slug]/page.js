0230e91d4c62a4df4db0ee9a519b40fca61365e5
dmr-911 <dewan.mizanur911@gmail.com>
2024-05-25 01:32:34 +0600

    feat: common header file integrated

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DynamicMeal = () => {
   return (
-    <div>DynamicMeal</div>
+    <div>DynamicMeal Details</div>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log("executing...");
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/community">Community</Link></p>
+      <p><Link href="/meals/meals-1">Meal 1</Link></p>
+      <p><Link href="/meals/share">Meals Share</Link></p>
     </main>
   );
 }
