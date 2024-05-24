38cc506ddf697462d9d10c77b376229bf0a2024c
dmr-911 <dewan.mizanur911@gmail.com>
2024-05-25 01:04:33 +0600

    feat: some assets , images to public folder, some routes and dynamic routes added

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DynamicMeal = () => {
+  return (
+    <div>DynamicMeal</div>
+  )
+}
+
+export default DynamicMeal
---
+++ app/meals/share/page.js
@@ -0,0 +1,12 @@
+import Common from '@/components/Common'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Common/>
+    </div>
+  )
+}
+
+export default page
