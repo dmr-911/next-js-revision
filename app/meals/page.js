17712073e63722f1f55f33008d241f3cb3acc47c
dmr-911 <dewan.mizanur911@gmail.com>
2024-05-25 22:33:34 +0600

    feat: meals page organised, meals grid created, meal item component created

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
 import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const Meals = () => {
-  return <div>Meals</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe, and have fun</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default Meals;
