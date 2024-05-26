f2de19755c9faf7967f319e51e1f372a23b3d4fa
dmr-911 <dewan.mizanur911@gmail.com>
2024-05-26 23:32:22 +0600

    feat: meal details page created

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/getMeals";
 
-const DynamicMeal = () => {
+const DynamicMeal = async ({ params }) => {
+  const meal = await getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>"); // over write meal instructions for actual line break
   return (
-    <div>DynamicMeal Details</div>
-  )
-}
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
 
-export default DynamicMeal
+export default DynamicMeal;
